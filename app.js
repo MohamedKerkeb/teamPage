5831698d0333ae5788d44ae73f776367c5a6f967
MohamedKerkeb <kerkeb.moha@gmail.com>
2020-09-09 22:34:20 +0200

    mise en place du site

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-const card = document.querySelectorAll(".card");
-const container = document.querySelector(".container");
+// const card = document.querySelectorAll(".card");
+// const container = document.querySelector(".container");
 
-card.forEach((c, i) => {
-  const newLocal = window.screen.width;
-  if (i % 2 && newLocal <= 520) {
-    card[i].classList.add("down");
-  }
-  if (newLocal > 520) {
-    card[1].classList.add("down");
-    card[4].classList.add("down");
-  }
-});
+// card.forEach((c, i) => {
+//   const newLocal = window.screen.width;
+//   if (i % 2 && newLocal <= 520) {
+//     card[i].classList.add("down");
+//   }
+//   if (newLocal > 520) {
+//     card[1].classList.add("down");
+//     card[4].classList.add("down");
+//   }
+// });
